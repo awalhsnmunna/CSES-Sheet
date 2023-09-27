03d742d85856c8fe51f9cf40da48c93819981166
Munna <awalhsnmunna@gmail.com>
2023-09-27 12:00:41 +0600

    Code

--- Introductory Problems/MissingNumber.cpp
+++ Introductory Problems/MissingNumber.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// We know that the sum of the first n natural numbers formula is NSum = n*(n+1)/2;
+//We can see from the question that in the array/vector from n natural numbers, one number is missing. 
+// So, if we sum the numbers in vector and subtract from the NSum, we will get the missing number.
+
 void solve()
 {
     int n, sum = 0;
