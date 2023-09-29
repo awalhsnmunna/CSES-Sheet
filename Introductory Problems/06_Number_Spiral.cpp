a08c8fb3ae3bb1eaea9519b58e23f3ac004c6a43
Munna <awalhsnmunna@gmail.com>
2023-09-29 20:44:18 +0600

    Code

---
+++ Introductory Problems/06_Number_Spiral.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long int row, col, ans;
+    cin >> row >> col;
+
+    if (row > col)
+    {
+        if (row % 2 == 0)
+        {
+            ans = (row * row) - (col - 1);
+            cout << ans << endl;
+        }
+        else
+        {
+            ans = (((row - 1) * (row - 1) + 1)) + (col - 1);
+            cout << ans << endl;
+        }
+    }
+    else
+    {
+        if (col % 2 == 0)
+        {
+            ans = (col - 1) * (col - 1) + 1 + (row - 1);
+            cout << ans << endl;
+        }
+        else
+        {
+            ans = (col) * (col) - (row - 1);
+            cout << ans << endl;
+        }
+    }
+}
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
